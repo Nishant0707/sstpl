523651ce42bd095a7d8c5c6404f4ee1186167e9b
Nishant0707 <ngautam535@gmail.com>
2025-08-08 18:43:01 +0530

    Fix case sensitivity for SereduBanner import

--- frontend/src/pages/Education.jsx
+++ frontend/src/pages/Education.jsx
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react';
 <<<<<<< HEAD
 import SereduBanner from '../components/Edu/SereduBanner';
 =======
-import SereduBanner from '../components/edu/SereduBanner';
+import SereduBanner from '../components/Edu/SereduBanner';
 >>>>>>> b8c46a7a46f986e62efdc105cb17900fce112222
 import SereduBlog from '../components/Edu/SereduBlog';
 import SereduTypeEA from '../components/Edu/SereduTypeEA';
